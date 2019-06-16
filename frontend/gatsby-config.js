cb85f2e27c86502b39efd72a3ba1cdf7117b0446
Andrew Horn <arhorn@smcm.edu>
2019-06-16 16:29:34 -0500

    moved frontend into specialized frontend folder

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `mars photo search app`,
     description: `Find some photos from the Curiousity rover on Mars.`,
-    author: `Andrew Horn`,
+    author: `Andrew Horn`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -10,8 +10,8 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -24,11 +24,11 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
-}
+  ]
+};
--- frontend/src/components/marsPhotoSearch/marsPhotoSearch.js
+++ frontend/src/components/marsPhotoSearch/marsPhotoSearch.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 
+// search for photos
+// inputs:
+//    Sol (mission day)
+//    Camera (select or radio buttons)
+// user clicks "find photos"
+//
+
 class MarsPhotoSearch extends Component {
   render() {
     return <h1>imma component</h1>;
